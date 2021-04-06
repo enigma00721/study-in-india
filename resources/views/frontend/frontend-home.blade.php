d28bc45ca3102989791d3a4de658cb6db37cd24d
Raju <you@example.com>
2021-04-06 13:52:24 +0545

    first commit

---
+++ resources/views/frontend/frontend-home.blade.php
@@ -0,0 +1,8 @@
+@extends('frontend.frontend-master')
+
+@section('content')
+
+@include('frontend.home-pages.home-02')
+{{-- @include('frontend.home-pages.home-'.get_static_option('home_page_variant')) --}}
+
+@endsection
