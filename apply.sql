3013acd891230c58119025f1e173b6a3757b1453
Raju <you@example.com>
2021-06-09 16:56:20 +0545

    online apply slq

--- apply.sql
+++ apply.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 09, 2021 at 01:52 PM
+-- Generation Time: Jun 09, 2021 at 04:54 PM
 -- Server version: 8.0.25
 -- PHP Version: 7.3.28-2+ubuntu18.04.1+deb.sury.org+1
 
@@ -50,6 +50,15 @@ CREATE TABLE `online_applies` (
 
 --
 -- Dumping data for table `online_applies`
+--
+
+INSERT INTO `online_applies` (`id`, `created_at`, `updated_at`, `name`, `mobile_number`, `gender`, `dob`, `email`, `address`, `district`, `parent_name`, `parent_relationship`, `parent_address`, `parent_occupation`, `parent_contact`, `level`, `discipline`, `university`, `course`) VALUES
+(2, '2021-04-24 20:42:45', '2021-04-24 20:42:45', 'Wallace Roach', '133', 'female', '2021-08-24', 'cold.knight21@gmail.com', 'Facilis laborum odit', 'Rerum facere et sit', 'Forrest Johns', 'Dolor adipisci ipsa', 'Sed aut magni tempor', 'Anim suscipit velit', '58', 'Bachelor', 'Science', 'Acharya & BM Reddy College of Pharmacy, Bengaluru', 'Bachelor of Engineering (B.E.)'),
+(3, '2021-05-07 12:01:15', '2021-05-07 12:01:15', 'Jena Wolfe', '111', 'male', '1983-04-19', 'demo@demo.com', 'Est quasi laboriosam', 'Eos aute ipsum opti', 'Quinlan Blake', 'Dolor cupidatat eum', 'Ipsum et laborum S', 'Vero quis ut quas in', '9323423432', '+2', 'Science', 'Andhra University', 'Bachelor of Engineering (B.E.)'),
+(4, '2021-05-07 12:02:44', '2021-05-07 12:02:44', 'Yael Kidd', '208', 'male', '2010-01-02', 'cold.knight21@gmail.com', 'Suscipit quis except', 'Lorem enim quasi ips', 'Medge French', 'Non adipisicing dict', 'Dolor ex id et cons', 'Maiores doloribus mo', '74', 'A Level', 'Humanities', 'Andhra University', 'Bachelor of Pharmacy (B.Pharm)');
+
+--
+-- Indexes for dumped tables
 --
 
 --
